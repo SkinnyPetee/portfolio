c0984b6bcc772769f7718b69260d6103d5f40bd8
sriram <sriram@srirams-Mac.local>
2018-07-02 08:13:20 -0700

    added create and new to ports

--- app/controllers/ports_controller.rb
+++ app/controllers/ports_controller.rb
@@ -4,5 +4,23 @@ class PortsController < ApplicationController
     @portfolio_items = Port.all
   end
 
+  def new
+    @portfolio_item = Port.new
+  end
+
+  def create
+    @portfolio_item = Port.new(params.require(:port).permit(:title,:subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to ports_path, notice: 'portfolio was successfully create.' }
+
+      else
+        format.html { render :new }
+
+      end
+    end
+  end
+
 
 end
